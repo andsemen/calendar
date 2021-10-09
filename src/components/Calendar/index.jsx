69e3b888cb3c4b951ca90fc7abc98c54ba94df3e
Andrey Semenchenko <andrew.semenchenko.94@gmail.com>
2021-10-09 11:29:00 +0300

    init

---
+++ src/components/Calendar/index.jsx
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+import styles from './calendar.module.scss'
+
+const Calendar = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Calendar;
